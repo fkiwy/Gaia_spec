4c42b5d1198e610b4f0e35bdbccad1c50a1d7a66
Frank Kiwy <54667906+fkiwy@users.noreply.github.com>
2023-02-09 18:26:11 +0100

    Update Spec_compare_exec.py

--- Spec_compare_exec.py
+++ Spec_compare_exec.py
@@ -2,7 +2,7 @@ from Spec_compare import compare_spectra
 
 sources = [58925512487888896, 378026395579692672, 200296663143599104, 1204754033223498624]  # K5, M0, M5, M9
 
-results = compare_spectra(sources, plot_spectrum=True, plot_template=True, spectrum_uncertainty=True, template_uncertainty=True, file_format='png')
+results = compare_spectra(sources, plot_spectrum=True, plot_template=True, spectrum_uncertainty=True, template_uncertainty=True, file_format='png', output_dir='output')
 
 for result in results:
     print('Gaia source id:', result[0], 'Matched tempalte:', result[2])
